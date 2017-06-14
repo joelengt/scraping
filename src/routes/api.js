494bcf53cfd01ac3aeadeeaec16268e5628148f1
joelengt <joelengt@gmail.com>
2017-06-14 15:45:28 -0500

    Change Access methods rotues

--- src/routes/api.js
+++ src/routes/api.js
@@ -7,10 +7,10 @@ var ApiControllerSource = new ApiController()
 var express = require('express')
 var router = express.Router()
 
-router.get('/brand', (req, res) => ApiControllerSource.getList(req, res))
-router.post('/brand', (req, res) => ApiControllerSource.create(req, res))
-router.get('/brand/:id', (req, res) => ApiControllerSource.getById(req, res))
-router.put('/brand/:id', (req, res) => ApiControllerSource.updateById(req, res))
-router.delete('/brand/:id', (req, res) => ApiControllerSource.deleteById(req, res))
+router.get('/brand', ApiControllerSource.getList)
+router.post('/brand', ApiControllerSource.create)
+router.get('/brand/:id', ApiControllerSource.getById)
+router.put('/brand/:id', ApiControllerSource.updateById)
+router.delete('/brand/:id', ApiControllerSource.deleteById)
 
 module.exports = router
