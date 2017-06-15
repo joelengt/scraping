3d294f68a074e88c7ab9e061082867a78d503f64
joelengt <joelengt@gmail.com>
2017-06-15 09:47:58 -0500

    Update unit testing- rotues

--- src/routes/__test__/index.test.js
+++ src/routes/__test__/index.test.js
@@ -9,17 +9,18 @@ var service = axios.create({
 
 let URI = 'http://' + process.env.HOST + ':' + process.env.PORT
 
-// test('it route create new ads item - POST /api/ads', async () => {
-//   let body = {
-//     name: 'pretty banner',
-//     link: '/something.png',
-//     photo: 'http://cdn.image./something.png'
-//   }
-//   let response = await service.post(URI + '/api/ads', body)
-//   expect(response.data).toMatchSnapshot()
-// })
+test('it route create new ads item - POST /api/ads', async () => {
+  let body = {
+    name: 'pretty banner',
+    link: '/something.png',
+    photo: 'http://cdn.image./something.png'
+  }
+  let response = await service.post(URI + '/api/ads', body)
+  expect(response.data).toMatchSnapshot()
+})
 
 test('it route ads item by id - GET /api/ads/:id', async () => {
+  // Get ultimes item created
   let response = await service.get(URI + '/api/ads/11')
   expect(response.data).toMatchSnapshot()
 })
@@ -39,7 +40,7 @@ test('it route update ads item by id - PUT /api/ads/:id', async () => {
   expect(response.data).toMatchSnapshot()
 })
 
-// test('it route delete ads item by id - DELETE /api/ads/:id', async () => {
-//   let response = await service.post(URI + '/api/ads/35?_method=delete')
-//   expect(response.data).toMatchSnapshot()
-// })
+test('it route delete ads item by id - DELETE /api/ads/:id', async () => {
+  let response = await service.post(URI + '/api/ads/35?_method=delete')
+  expect(response.data).toMatchSnapshot()
+})
